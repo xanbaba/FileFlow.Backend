80715e02fab10db85b7b2735fdff331c6f34def1
Khanbaba <28xanbabaf@gmail.com>
2025-05-22 15:08:39 +0400

    Add application initialization and message bus setup
    Introduce `UseApplication` extension method to ensure database creation during app startup. Configure scoped `IEventBus` service and MediatR for improved messaging and event handling.

--- FileFlow.Api/Program.cs
+++ FileFlow.Api/Program.cs
@@ -53,6 +53,8 @@ if (app.Environment.IsDevelopment())
     app.MapOpenApi();
 }
 
+app.UseApplication();
+
 app.UseHttpsRedirection();
 
 app.UseCors();
--- FileFlow.Application/HostApplicationBuilderExtensions.cs
+++ FileFlow.Application/HostApplicationBuilderExtensions.cs
@@ -1,4 +1,5 @@
 using FileFlow.Application.Database;
+using FileFlow.Application.MessageBus;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -14,5 +15,16 @@ public static class HostApplicationBuilderExtensions
         {
             optionsBuilder.UseSqlServer(builder.Configuration.GetConnectionString("SqlServer"));
         });
+
+        builder.Services.AddScoped<IEventBus, EventBus>();
+        builder.Services.AddMediatR(x => x.RegisterServicesFromAssemblyContaining<AssemblyMarker>());
+    }
+
+    public static void UseApplication(this IHost app)
+    {
+        using var serviceScope = app.Services.CreateScope();
+
+        var appDbContext = serviceScope.ServiceProvider.GetRequiredService<AppDbContext>();
+        appDbContext.Database.EnsureCreated();
     }
 }
