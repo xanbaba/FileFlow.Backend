3910f193680fffbd5f7185ac5cba951a58730227
Khanbaba <28xanbabaf@gmail.com>
2025-05-24 11:15:12 +0400

    Remove FolderDetailsResponse class definition
    The FolderDetailsResponse class is no longer needed and has been deleted. This simplifies the codebase and removes unused resources to improve maintainability.

--- FileFlow.Contracts/Responses/FolderDetailsResponse.cs
+++
@@ -1,7 +0,0 @@
-namespace FileFlow.Contracts.Responses;
-
-public class FolderDetailsResponse
-{
-    public FileFolderResponse Folder { get; set; } = null!;
-    public ICollection<FileFolderResponse> Children { get; set; } = new List<FileFolderResponse>();
-}
