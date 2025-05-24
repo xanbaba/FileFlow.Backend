fe6a6ffb7596d41a59a983c53b3becfad7232f66
Khanbaba <28xanbabaf@gmail.com>
2025-05-24 11:57:50 +0400

    Refactor file upload endpoint to use UploadFileForm model
    Replaced the UploadFileRequest model with an inline UploadFileForm class to simplify data handling. Updated the endpoint to accept a multipart form with the new model, ensuring better alignment with form data structure.

--- FileFlow.Api/Endpoints/FileEndpoints/UploadFileEndpoint.cs
+++ FileFlow.Api/Endpoints/FileEndpoints/UploadFileEndpoint.cs
@@ -1,28 +1,34 @@
 using System.Security.Claims;
 using FileFlow.Application.Services.Abstractions;
-using FileFlow.Contracts.Requests;
 using FileFlow.Contracts.Responses;
+using Microsoft.AspNetCore.Mvc;
 
 namespace FileFlow.Api.Endpoints.FileEndpoints;
+
+public class UploadFileForm
+{
+    public required IFormFile File { get; set; } = null!;
+
+    public required string TargetFolderPath { get; set; } = null!;
+}
 
 public class UploadFileEndpoint : IEndpoint
 {
     public void Map(IEndpointRouteBuilder builder)
     {
         builder.MapPost(Contracts.Endpoints.FileEndpoints.UploadFile,
-                async (IFormFile file, UploadFileRequest request, IFileService fileService, ClaimsPrincipal user,
+                async ([FromForm] UploadFileForm uploadFileForm, IFileService fileService, ClaimsPrincipal user,
                     CancellationToken cancellationToken) =>
                 {
-                    var uploadedFile = await fileService.UploadAsync(user.GetUserid(), file.FileName, request.TargetFolderPath,
-                        file.OpenReadStream(), cancellationToken);
-                    
+                    var uploadedFile = await fileService.UploadAsync(user.GetUserid(), uploadFileForm.File.FileName,
+                        uploadFileForm.TargetFolderPath,
+                        uploadFileForm.File.OpenReadStream(), cancellationToken);
+
                     return Results.Ok(uploadedFile.ToResponse());
-                    
                 })
             .WithName(Name)
             .RequireAuthorization()
-            .Accepts<IFormFile>("multipart/form-data")
-            .Produces(StatusCodes.Status200OK)
+            .Produces<FileFolderResponse>()
             .Produces(StatusCodes.Status401Unauthorized)
             .Produces(StatusCodes.Status400BadRequest)
             .Produces(StatusCodes.Status404NotFound);
--- FileFlow.Contracts/Requests/UploadFileRequest.cs
+++
@@ -1,6 +0,0 @@
-namespace FileFlow.Contracts.Requests;
-
-public class UploadFileRequest
-{
-    public string? TargetFolderPath { get; set; }
-}
