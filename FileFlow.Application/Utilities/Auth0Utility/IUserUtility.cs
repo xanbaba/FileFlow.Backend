46620f56978ceaabbe6e0b9618bf3e00c689a801
Khanbaba <28xanbabaf@gmail.com>
2025-05-27 16:27:42 +0400

    Refactor GetUserEmailAsync to support cancellation tokens
    Added support for `CancellationToken` in `GetUserEmailAsync` methods to improve responsiveness and cancellation handling. Enhanced error handling to return null for 404 responses, ensuring better fault tolerance.

--- FileFlow.Application/Utilities/Auth0Utility/IUserUtility.cs
+++ FileFlow.Application/Utilities/Auth0Utility/IUserUtility.cs
@@ -2,5 +2,5 @@ namespace FileFlow.Application.Utilities.Auth0Utility;
 
 internal interface IUserUtility
 {
-    public Task<string> GetUserEmailAsync(string userId);
+    public Task<string?> GetUserEmailAsync(string userId, CancellationToken cancellationToken = default);
 }
--- FileFlow.Application/Utilities/Auth0Utility/UserUtility.cs
+++ FileFlow.Application/Utilities/Auth0Utility/UserUtility.cs
@@ -15,27 +15,31 @@ internal class UserUtility : IUserUtility
         _httpClient = httpClientFactory.CreateClient("Auth0Client");
     }
     
-    public async Task<string> GetUserEmailAsync(string userId)
+    public async Task<string?> GetUserEmailAsync(string userId, CancellationToken cancellationToken = default)
     {
-        var response = await GetUserEmailAsync(userId, await _auth0AccessTokenProvider.GetAccessTokenAsync());
+        var response = await GetUserEmailAsync(userId, await _auth0AccessTokenProvider.GetAccessTokenAsync(), cancellationToken);
         if (response.StatusCode == HttpStatusCode.Unauthorized)
         {
             await _auth0AccessTokenProvider.RefreshAccessTokenAsync();
-            response = await GetUserEmailAsync(userId, await _auth0AccessTokenProvider.GetAccessTokenAsync());
+            response = await GetUserEmailAsync(userId, await _auth0AccessTokenProvider.GetAccessTokenAsync(), cancellationToken);
         }
 
+        if (response.StatusCode == HttpStatusCode.NotFound)
+        {
+            return null;
+        }
         response.EnsureSuccessStatusCode();
-        var content = await response.Content.ReadAsStringAsync();
+        var content = await response.Content.ReadAsStringAsync(cancellationToken);
         var deserializedContent = JsonSerializer.Deserialize<dynamic>(content);
         return deserializedContent!.email;
     }
 
-    private async Task<HttpResponseMessage> GetUserEmailAsync(string userId, string accessToken)
+    private async Task<HttpResponseMessage> GetUserEmailAsync(string userId, string accessToken, CancellationToken cancellationToken = default)
     {
         var request = new HttpRequestMessage(HttpMethod.Get, $"/api/v2/users/{userId}");
         request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
         request.Headers.Add("Accept", "application/json");
-        var response = await _httpClient.SendAsync(request);
+        var response = await _httpClient.SendAsync(request, cancellationToken);
         return response;
     }
 }
