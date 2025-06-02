6892cef34a3bb35a82446b6ebc82afeb670a34a7
Khanbaba <28xanbabaf@gmail.com>
2025-06-02 15:47:15 +0400

    Make Path index in FileFolder unique for consistency and data integrity

---
+++ FileFlow.Api/Migrations/20250601201409_MakePathIndexOfFileFolderUnique.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FileFlow.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakePathIndexOfFileFolderUnique : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_FileFolders_Path",
+                table: "FileFolders");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FileFolders_Path",
+                table: "FileFolders",
+                column: "Path",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_FileFolders_Path",
+                table: "FileFolders");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FileFolders_Path",
+                table: "FileFolders",
+                column: "Path");
+        }
+    }
+}
